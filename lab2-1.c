b52ca896889d3dfbd06f37250177638e17169ecd
Proverbs13 <ichan0318@naver.com>
2022-03-20 03:19:12 +0900

    Submit assignments

--- lab2-1.c
+++ lab2-1.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
-int main()
-{
-    char charType;
-    int integerType;
-    float floatType;
-    double doubleType;
+int main(){
+    printf("[----- [이찬] [2019038029] -----]\n");
+    char charType;    //char형 변수 charType 선언
+    int integerType;  //int형 변수 integerType 선언
+    float floatType;  //float형 변수 floatType 선언
+    double doubleType;//double형 변수 doubleType 선언
 
-    printf("Size of char: %ld byte\n", sizeof(charType));
-    printf("Size of int: %ld bytes\n", sizeof(integerType));
-    printf("Size of float: %ld bytes\n", sizeof(floatType));
-    printf("Size of double: %ld bytes\n", sizeof(doubleType));
-
+    printf("Size of char: %ld byte\n", sizeof(charType));     //char형 변수 charType의 크기=1바이트 출력 
+    printf("Size of int: %ld bytes\n", sizeof(integerType));  //int형 변수 integerType의 크기=4바이트 출력
+    printf("Size of float: %ld bytes\n", sizeof(floatType));  //float형 변수 floatType의 크기=4바이트 출력
+    printf("Size of double: %ld bytes\n", sizeof(doubleType));//double형 변수 doubleType의 크기=8바이트 출력
     printf("-----------------------------------------\n");
-    
-    printf("Size of char: %ld byte\n", sizeof(char));
-    printf("Size of int: %ld bytes\n", sizeof(int));
-    printf("Size of float: %ld bytes\n", sizeof(float));
-    printf("Size of double: %ld bytes\n", sizeof(double));
-
+    printf("Size of char: %ld byte\n", sizeof(char));     // char 자료형 크기=1바이트 출력
+    printf("Size of int: %ld bytes\n", sizeof(int));      // int 자료형 크기=4바이트 출력
+    printf("Size of float: %ld bytes\n", sizeof(float));  // float 자료형 크기=4바이트 출력
+    printf("Size of double: %ld bytes\n", sizeof(double));// double 자료형 크기=8바이트 출력
     printf("-----------------------------------------\n");
 
-    printf("Size of char*: %ld byte\n", sizeof(char *));
-    printf("Size of int*: %ld bytes\n", sizeof(int *));
-    printf("Size of float*: %ld bytes\n", sizeof(float *));
-    printf("Size of double*: %ld bytes\n", sizeof(double *));
+    //vscode x64는 32비트 시스템(프로그램), 32비트 시스템에서는 자료형에 상관없이
+    //~형 공간을 가리키는 포인터의 크기는 32비트의 주소를 받을 4바이트 (4바이트=32비트)
+    printf("Size of char*: %ld byte\n", sizeof(char *));     //char형 공간 가리키는 포인터 자료형의 크기=4바이트 출력
+    printf("Size of int*: %ld bytes\n", sizeof(int *));      //int형 공간 가리키는 포인터 자료형의 크기=4바이트 출력
+    printf("Size of float*: %ld bytes\n", sizeof(float *));  //float형 공간 가리키는 포인터 자료형의 크기=4바이트 출력
+    printf("Size of double*: %ld bytes\n", sizeof(double *));//double형 공간 가리키는 포인터 자료형의 크기=4바이트 출력
     return 0;
 }
--- lab2-2.c
+++ lab2-2.c
@@ -1,42 +1,54 @@
 #include <stdio.h>
 int main()
 {
-    int i;
-    int *ptr;
-    int **dptr;
+    printf("[----- [이찬] [2019038029] -----]\n");
+
+    int i;     //int형 변수 i 선언
+    int *ptr;  //int형 포인터 변수 ptr 선언 
+    int **dptr;//int형 이중포인터 변수 dptr 선언
+
     i = 1234;
+
     printf("[checking values before ptr = &i] \n");
-    printf("value of i == %d\n", i);
-    printf("address of i == %p\n", &i);
-    printf("value of ptr == %p\n", ptr);
-    printf("address of ptr == %p\n", &ptr);
+    printf("value of i == %d\n", i);       //i의 값 = 1234 출력
+    printf("address of i == %p\n", &i);    //i의 주소값 출력
+    printf("value of ptr == %p\n", ptr);   //ptr의 값은 초기화가 되지않아 쓰레기값 출력
+    printf("address of ptr == %p\n", &ptr);//ptr의 주소값 출력
+
     ptr = &i; /* ptr is now holding the address of i */
+    //ptr에 i의 주소값 저장
     printf("\n[checking values after ptr = &i] \n");
-    printf("value of i == %d\n", i);
-    printf("address of i == %p\n", &i);
-    printf("value of ptr == %p\n", ptr);
-    printf("address of ptr == %p\n", &ptr);
-    printf("value of *ptr == %d\n", *ptr);
+    printf("value of i == %d\n", i);       //i의 값 = 1234 출력
+    printf("address of i == %p\n", &i);    //i의 주소값 출력
+    printf("value of ptr == %p\n", ptr);   //ptr의 값 = i의 주소값 출력
+    printf("address of ptr == %p\n", &ptr);//ptr의 주소값 출력
+    printf("value of *ptr == %d\n", *ptr); //ptr이 가리키는 i의 값 출력
+
     dptr = &ptr; /* dptr is now holding the address of ptr */
+    //dptr에 ptr의 주소값 저장
     printf("\n[checking values after dptr = &ptr] \n");
-    printf("value of i == %d\n", i);
-    printf("address of i == %p\n", &i);
-    printf("value of ptr == %p\n", ptr);
-    printf("address of ptr == %p\n", &ptr);
-    printf("value of *ptr == %d\n", *ptr);
-    printf("value of dptr == %p\n", dptr);
-    printf("address of dptr == %p\n", &dptr);
-    printf("value of *dptr == %p\n", *dptr);
-    printf("value of **dptr == %d\n", **dptr);
+    printf("value of i == %d\n", i);          //i의 값 = 1234 출력
+    printf("address of i == %p\n", &i);       //i의 주소값 출력
+    printf("value of ptr == %p\n", ptr);      //ptr의 값 = i의 주소값 출력
+    printf("address of ptr == %p\n", &ptr);   //ptr의 주소값 출력
+    printf("value of *ptr == %d\n", *ptr);    //ptr이 가리키는 i의 값 출력
+    printf("value of dptr == %p\n", dptr);    //dptr의값 = ptr의 주소값 출력
+    printf("address of dptr == %p\n", &dptr); //dptr의 주소값 출력
+    printf("value of *dptr == %p\n", *dptr);  //dptr이 가리키는 ptr의 값 = i의 주소값 출력
+    printf("value of **dptr == %d\n", **dptr);//dptr이 가리키는 ptr이 가리키는 값=i의 값=1234 출력
+                                                         
     *ptr = 7777; /* changing the value of *ptr */
+    // *ptr은 ptr이 가리키는 값, 즉 i의 값이기 때문에 i = 7777이 된다.
     printf("\n[after *ptr = 7777] \n");
-    printf("value of i == %d\n", i);
-    printf("value of *ptr == %d\n", *ptr);
-    printf("value of **dptr == %d\n", **dptr);
+    printf("value of i == %d\n", i);          // 변한 i의 값 = 7777 출력
+    printf("value of *ptr == %d\n", *ptr);    // ptr이 가리키는 값 = i의 값 출력
+    printf("value of **dptr == %d\n", **dptr);// dptr이 가리키는 ptr이 가리키는 값= i의 값 출력
+
     **dptr = 8888; /* changing the value of **dptr */
-    printf("\n[after **dptr = 8888] \n");
-    printf("value of i == %d\n", i);
-    printf("value of *ptr == %d\n", *ptr);
-    printf("value of **dptr == %d\n", **dptr);
+    // **dptr은 dptr이 가리키는 값-> dtr이 가리키는 값, 즉 i의 값이기 때문에 i = 8888이 된다.
+    printf("\n[after **dptr = 8888] \n"); 
+    printf("value of i == %d\n", i);          // 변한 i의 값 = 8888 출력
+    printf("value of *ptr == %d\n", *ptr);    // ptr이 가리키는 값 = i의 값 출력
+    printf("value of **dptr == %d\n", **dptr);// dptr이 가리키는 ptr이 가리키는 값= i의 값 출력
     return 0;
 }
